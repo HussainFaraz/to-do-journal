c45da4c7ec2ff255481c1e53d351fc901b411221
Faraz <farazhusain5148@gmail.com>
2021-06-19 16:32:28 +0530

    deployment changes

--- TaskManagement/settings.py
+++ TaskManagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8&2rd4u8w+5+^&+scu@8086!1(!z^oiezimfe(g^9-=ho1npc_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["to-do-journal.herokuapp.com" , "localhost"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
